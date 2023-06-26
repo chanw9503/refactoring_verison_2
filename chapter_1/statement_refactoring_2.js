af4fc5f6cb101991bce2e1cf4012f63e47d03ec3
ChanwooPark <chanw9503@gmail.com>
2023-06-26 20:21:18 +0900

    중간 데이터 분리하기

--- chapter_1/statement_refactoring_2.js
+++ chapter_1/statement_refactoring_2.js
@@ -12,11 +12,13 @@ const invoices = require('./invoices.json');
 const plays = require('./plays.json');
 
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
-  let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+function renderPlainText(data, invoice, plays) {
+  let result = `청구 내역 (고객명 : ${data.customer})\n`;
 
   for (let perf of invoice.performances) {
     //청구 내역을 출력한다.
