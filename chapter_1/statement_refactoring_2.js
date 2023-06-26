dfa855582e9995329da0f8e94b052dbf1ec1667c
ChanwooPark <chanw9503@gmail.com>
2023-06-26 17:39:50 +0900

    volumeCredits 지역변수 없애기

--- chapter_1/statement_refactoring_2.js
+++ chapter_1/statement_refactoring_2.js
@@ -36,6 +36,13 @@ function amountFor(aPerformance) {
   return result;
 }
 
+function volumeCreditsFor(perf) {
+  let volumeCredits = 0;
+  volumeCredits += Math.max(perf.audience - 30, 0);
+  if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+  return volumeCredits;
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -48,17 +55,14 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
-
     //포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-
-    //희극 관격 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     //청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
-    totalAmount += thisAmount;
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
