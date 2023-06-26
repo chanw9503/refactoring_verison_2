974e97b3f3b3b3f62fa625bd03ad19a76faf17d6
ChanwooPark <chanw9503@gmail.com>
2023-06-26 20:13:44 +0900

    totalAmount 질의함수로 분리

--- chapter_1/statement_refactoring_2.js
+++ chapter_1/statement_refactoring_2.js
@@ -64,15 +64,25 @@ function totalVolumeCredit() {
   return volumeCredits;
 }
 
-function statement(invoice, plays) {
+function appleSauce() {
   let totalAmount = 0;
+
+  for (let perf of invoices.performances) {
+    totalAmount += amountFor(perf);
+  }
+
+  return totalAmount;
+}
+
+function statement(invoice, plays) {
   let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
     //청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf);
   }
+
+  const totalAmount = appleSauce();
 
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${totalVolumeCredit()}점\n`;
