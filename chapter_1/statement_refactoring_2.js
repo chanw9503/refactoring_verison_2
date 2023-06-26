f020f00aa153ee2198c6fd54c39d988f295fdee5
ChanwooPark <chanw9503@gmail.com>
2023-06-26 20:18:48 +0900

    구조 분리를 위해 중간 데이터 구조 생성

--- chapter_1/statement_refactoring_2.js
+++ chapter_1/statement_refactoring_2.js
@@ -11,6 +11,24 @@
 const invoices = require('./invoices.json');
 const plays = require('./plays.json');
 
+function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
+  let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+
+  for (let perf of invoice.performances) {
+    //청구 내역을 출력한다.
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+  }
+
+  result += `총액: ${usd(appleSauce())}\n`;
+  result += `적립 포인트: ${totalVolumeCredit()}점\n`;
+
+  return result;
+}
+
 function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
@@ -70,20 +88,6 @@ function appleSauce() {
   for (let perf of invoices.performances) {
     result += amountFor(perf);
   }
-
-  return result;
-}
-
-function statement(invoice, plays) {
-  let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
-
-  for (let perf of invoice.performances) {
-    //청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-  }
-
-  result += `총액: ${usd(appleSauce())}\n`;
-  result += `적립 포인트: ${totalVolumeCredit()}점\n`;
 
   return result;
 }
