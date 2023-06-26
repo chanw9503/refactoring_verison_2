9560ab8df38fe64b4bafaeb55eee3410fdccd096
ChanwooPark <chanw9503@gmail.com>
2023-06-26 17:31:16 +0900

    amounFor()에 함수 선언 바꾸기

--- chapter_1/statement_refactoring_2.js
+++ chapter_1/statement_refactoring_2.js
@@ -14,7 +14,7 @@ function playFor(aPerformance) {
 function amountFor(aPerformance, play) {
   let result = 0;
 
-  switch (play.type) {
+  switch (playFor(aPerformance).type) {
     case 'tragedy': //비극
       result = 40000;
       if (aPerformance.audience > 30) {
@@ -30,7 +30,7 @@ function amountFor(aPerformance, play) {
       result += 300 * aPerformance.audience;
       break;
     default:
-      throw new Error(`알 수 없는 장르 : ${play.type}`);
+      throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
   }
 
   return result;
