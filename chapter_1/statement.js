ce71454d1133fc775e961095818e1249ef9514ea
ChanwooPark <chanw9503@gmail.com>
2023-07-01 04:36:37 +0900

    html render 추가

--- chapter_1/statement.js
+++ chapter_1/statement.js
@@ -9,14 +9,12 @@
  */
 
 import createStatementData from './createStatementData.js';
-import { createRequire } from 'module';
-import invoices from './invoices.js';
-import plays from './plays.js';
+// import invoices from './invoices.js';
+// import plays from './plays.js';
 
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
-
 function renderPlainText(data) {
   let result = `청구 내역 (고객명 : ${data.customer})\n`;
 
@@ -24,9 +22,31 @@ function renderPlainText(data) {
     //청구 내역을 출력한다.
     result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
+  yy;
 
   result += `총액: ${usd(data.totalAmount)}\n`;
   result += `적립 포인트: ${data.totalVolumeCredit}점\n`;
+  return result;
+}
+
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명 : ${data.customer}<h1/>\n`;
+  result += '<table>\n';
+  result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>';
+
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${perf.amount}</td></tr>\n`;
+  }
+
+  result += '</table>\n';
+  result += `<p>총액 : <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트 <em>${data.totalVolumeCredit}</em>점</p>\n`;
+
   return result;
 }
 
@@ -38,5 +58,5 @@ function usd(aNumber) {
   }).format(aNumber / 100);
 }
 
-const result = statement(invoices, plays);
-console.log(result);
+// const result = statement(invoices, plays);
+// console.log(result);
